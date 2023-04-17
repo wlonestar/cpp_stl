1a17a2b7c34ef5ae45ce13e29a26ed66a41fd711
wlonestar <wlonestar@163.com>
2023-04-17 10:36:34 +0800

    finish vector implementation

--- include/vector.h
+++ include/vector.h
@@ -18,16 +18,48 @@
 
 namespace stl {
 
+/**
+ * stl::vector is a sequence container that encapsulates dynamic size arrays.
+ *
+ * The elements are stored contiguously, which means that elements can be
+ * accessed not only through iterators, but also using offsets to regular
+ * pointers to elements. This means that a pointer to an element of a vector
+ * may be passed to any function that expects a pointer to an element of an
+ * array.
+ *
+ * The storage of the vector is handled automatically, being expanded as needed.
+ * Vectors usually occupy more space than static arrays, because more memory is
+ * allocated to handle future growth. This way a vector does not need to
+ * reallocate each time an element is inserted, but only when the additional
+ * memory is exhausted. The total amount of allocated memory can be queried
+ * using capacity() function. Extra memory can be returned to the system via a
+ * call to shrink_to_fit().
+ *
+ * Reallocations are usually costly operations in terms of performance.
+ * The reserve() function can be used to eliminate reallocations if the number
+ * of elements is known beforehand.
+ *
+ * The complexity (efficiency) of common operations on vectors is as follows:
+ *  - Random access - constant O(1)
+ *  - Insertion or removal of elements at the end - amortized constant O(1)
+ *  - Insertion or removal of elements - linear in the distance to the end of the
+ *    vector O(n)
+ */
+
 template<class T>
 class vector {
 public:
+  /**
+   * Member types
+   */
+
   typedef T value_type;
   typedef std::size_t size_type;
   typedef std::ptrdiff_t difference_type;
-  typedef value_type&  reference;
-  typedef const value_type& const_reference;
-  typedef value_type* pointer;
-  typedef const value_type * const_pointer;
+  typedef value_type &reference;
+  typedef const value_type &const_reference;
+  typedef value_type *pointer;
+  typedef const value_type *const_pointer;
 
   class iterator {
     using value_type = T;
@@ -175,44 +207,115 @@ private:
   bool range_check(size_type n);
 
 public:
+  /**
+   * Member functions
+   */
+
+  /**
+   * Default constructor. Constructs an empty container.
+   */
   constexpr vector();
+
+  /**
+   * Constructs the container with @count copies of elements with value @value.
+   *
+   * @count - the size of the container
+   * @value - the value to initialize elements of the container with
+   */
   constexpr vector(size_type count, const T &value);
+
+  /**
+   * Constructs the container with @count default-inserted instances of T.
+   * No copies are made.
+   *
+   * @count - the size of the container
+   */
   constexpr explicit vector(size_type count);
+
+  /**
+   * Copy constructor. Constructs the container with the copy of the contents
+   * of @other.
+   *
+   * @other - another container to ber used as source to initialize the elements
+   *          of the container with
+   */
   constexpr vector(const vector &other);
   constexpr vector(const vector &&other) noexcept;
+
+  /**
+   * Constructs the container with the contents of the initializer list @init.
+   *
+   * @init - initializer list to initialize the elements of the container with
+   */
   constexpr vector(std::initializer_list<T> init);
+
+  /**
+   * Destructs the vector. The destructors of the elements are called and
+   * the used storage is deallocated.
+   *
+   * Note, that if the elements are pointers, the pointed-to objects are not
+   * destroyed.
+   */
   constexpr ~vector();
 
+  /**
+   * Copy assignment operator. Replaces the contents with a copy of the contents
+   * of @other.
+   *
+   * @other -	another container to use as data source
+   * @return - *this
+   */
   constexpr vector &operator=(const vector &other);
+
+  /**
+   * Move assignment operator. Replaces the contents with those of other using
+   * move semantics. @other is in a valid but unspecified state afterwards.
+   *
+   * @other -	another container to use as data source
+   * @return - *this
+   */
   constexpr vector &operator=(vector &&other) noexcept;
+
+  /**
+   * Replaces the contents with those identified by initializer list @ilist.
+   *
+   * @ilist - initializer list to use as data source
+   * @return - *this
+   */
   constexpr vector &operator=(std::initializer_list<T> ilist);
 
   /**
-   * Replace the contents with @count copies of value @value
+   * Replace the contents with @count copies of value @value.
    *
    * @count - the new size of the container
    * @value - the value to initialize elements of the container with
    */
   constexpr void assign(size_type count, const T &value);
+
   /**
-   * Replace the contents with the elements from the initializer list @ilist
+   * Replace the contents with the elements from the initializer list @ilist.
    *
    * @ilist - initializer list to copy the values from
    */
   constexpr void assign(std::initializer_list<T> ilist);
 
   /**
-   * Returns a reference to the element at specified location @pos, with bounds checking.
+   * Element access
+   */
+
+  /**
+   * Returns a reference to the element at specified location @pos,
+   * with bounds checking.
    *
    * @pos - position of the element to return
    * @return - reference to the requested element
-   * @exception - std::out_of_range
    */
   constexpr reference at(size_type pos);
   constexpr const_reference at(size_type pos) const;
 
   /**
-   * Returns a reference to the element at specified location @pos. No bounds checking is performed.
+   * Returns a reference to the element at specified location @pos.
+   * No bounds checking is performed.
    *
    * @pos - position of the element to return
    * @return - reference to the requested element
@@ -220,8 +323,19 @@ public:
   constexpr reference operator[](size_type pos);
   constexpr const_reference operator[](size_type pos) const;
 
+  /**
+   * Returns a reference to the first element in the container.
+   *
+   * @return - reference to the first element
+   */
   constexpr reference front();
   constexpr const_reference front() const;
+
+  /**
+   * Returns a reference to the last element in the container.
+   *
+   * @return - reference to the last element
+   */
   constexpr reference back();
   constexpr const_reference back() const;
 
@@ -231,7 +345,7 @@ public:
    * a valid range, even if the container is empty(data() is not
    * dereferenceable in that case).
    *
-   * @return - Pointer to the underlying element storage, For non-empty
+   * @return - Pointer to the underlying element storage. For non-empty
    * containers, the returned pointer compares equal to the address of
    * the first element
    *
@@ -240,12 +354,62 @@ public:
   constexpr T *data() noexcept;
   constexpr const T *data() const noexcept;
 
+  /**
+   * Iterators
+   */
+
+  /**
+   * Returns an iterator to first element of the vector.
+   * If the vector is empty, the returned iterator will be equal to end().
+   *
+   * @return - iterator to the first element
+   */
   constexpr iterator begin() noexcept;
+
+  /**
+   * Returns an iterator to the element following the last element of the vector.
+   * This element acts as a placeholder; attempting to access it results in
+   * undefined behavior.
+   *
+   * @return - iterator to the element following the last element.
+   */
   constexpr iterator end() noexcept;
+
+  /**
+   * Returns a reverse iterator to the first element of the reserved vector.
+   * It corresponds to the last element of the non-reversed vector.
+   * If the vector is empty, the returned iterator is equal to rend().
+   *
+   * @return - reverse iterator to the first element
+   */
   constexpr reverse_iterator rbegin() noexcept;
+
+  /**
+   * Returns a reverse iterator to the element following the last element of the
+   * reversed vector. It corresponds to the element preceding the first element
+   * of the non-reversed vector. This element acts as a placeholder, attempting
+   * to access it results in undefined behavior.
+   *
+   * @return - reverse iterator to the element following the last element
+   */
   constexpr reverse_iterator rend() noexcept;
 
+  /**
+   * Capacity
+   */
+
+  /**
+   * Check if the container has no elements.
+   *
+   * @return - true if the container is empty, false otherwise
+   */
   [[nodiscard]] constexpr bool empty() const noexcept;
+
+  /**
+   * Returns the number of the elements in the container.
+   *
+   * @return - the number of elements in the container
+   */
   [[nodiscard]] constexpr size_type size() const noexcept;
 
   /**
@@ -267,15 +431,39 @@ public:
    * @new_cap - new capacity of the vector, in number of elements
    */
   constexpr void reserve(size_type new_cap);
+
+  /**
+   * Returns the number of elements that the container has currently allocated
+   * space for.
+   *
+   * @return - capacity of the currently allocated storage
+   */
   [[nodiscard]] constexpr size_type capacity() const noexcept;
+
+  /**
+   * Requests the removal of unused capacity.
+   *
+   * It is a non-binding requests to reduce capacity() to size(). It depends on
+   * the implementation whether the request is fulfilled.
+   *
+   * If reallocation occurs, all iterators, including the past the end iterator,
+   * and all references to the elements are invalidated. If no reallocation takes
+   * place, no iterator or references are invalidated.
+   */
   constexpr void shrink_to_fit();
+
+  /**
+   * Modifiers
+   */
 
   /**
    * Erases all elements from the container. After this call, size() returns
    * zero.
+   *
    * Invalidates any references, pointers, or iterators referring to contained
    * elements. Any past-the-end iterators are also invalidated.
-   * Leaves the capacity() of the vector unchanged
+   *
+   * Leaves the capacity() of the vector unchanged.
    */
   constexpr void clear() noexcept;
 
@@ -287,6 +475,7 @@ public:
    * @return - pointer to the inserted @value
    */
   constexpr iterator insert(iterator pos, const T &value);
+
   /**
    * Inserts elements at the specified location in the container.
    *
@@ -295,6 +484,7 @@ public:
    * @return - pointer to the inserted @value
    */
   constexpr iterator insert(iterator pos, T &&value);
+
   /**
    * Inserts elements at the specified location in the container.
    *
@@ -304,6 +494,7 @@ public:
    * @return - pointer to the inserted @value
    */
   constexpr iterator insert(iterator pos, size_type count, const T &value);
+
   /**
    * Inserts elements at the specified location in the container.
    *
@@ -332,6 +523,7 @@ public:
    *    is returned.
    */
   constexpr iterator erase(iterator pos);
+
   /**
    * Erases the specified elements from the container.
    * Removes the elements in the range [@first, @last).
@@ -388,6 +580,7 @@ public:
    * @count - new size of the container
    */
   constexpr void resize(size_type count);
+
   /**
    * Resizes the container to contain @count element, does nothing
    * if @count == size().
@@ -595,12 +788,9 @@ constexpr vector<T> &vector<T>::operator=(const vector &other) {
 
 template<class T>
 constexpr vector<T> &vector<T>::operator=(vector &&other) noexcept {
-  _capacity = other._capacity;
-  _size = other._size;
-  _elem = new T[_capacity];
-  for (size_type i = 0; i < other._size; i++) {
-    _elem[i] = other._elem[i];
-  }
+  _capacity = std::move(other._capacity);
+  _size = std::move(other._size);
+  _elem = std::move(other._elem);
   return *this;
 }
 

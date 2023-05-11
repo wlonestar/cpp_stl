440315315065f0917e918392c9e19192c92c1bc0
wlonestar <wlonestar@163.com>
2023-05-11 13:56:15 +0800

    remove useless rbtree node algo

--- include/tree.h
+++ include/tree.h
@@ -28,14 +28,6 @@ enum rb_color {
 };
 
 /**
- * rbtree side type
- */
-enum rb_side {
-  RB_LEFT,
-  RB_RIGHT,
-};
-
-/**
  * rbtree node type
  */
 template<class T>
@@ -51,117 +43,14 @@ public:
   T value;
 
 public:
-  rbtree_node();
+  rbtree_node()
+      : parent(nullptr), left(nullptr), right(nullptr), color(RB_RED) {}
+
   rbtree_node(const T &val, rbtree_node<T> *parent = nullptr,
               rbtree_node<T> *left = nullptr, rbtree_node<T> *right = nullptr,
-              rb_color color = RB_RED);
-
-  rbtree_node<T> *insert(const T &val, rb_side side);
-  rbtree_node<T> *attach(rbtree_node<T> *p, rb_side side);
-
-  /**
-   * Debug
-   */
-
-  void print() {
-    std::cout << "node(" << value << ", " << this << "): parent: "
-              << parent << ", left: " << left << ", right: " << right << "\n";
-  }
+              rb_color color = RB_RED)
+      : value(val), parent(parent), left(left), right(right), color(color) {}
 };
-
-/**
- * rbtree node functions
- */
-
-template<class T>
-rbtree_node<T> *rb_min_child(rbtree_node<T> *p);
-template<class T>
-rbtree_node<T> *rb_max_child(rbtree_node<T> *p);
-template<class T>
-rbtree_node<T> *rb_increment(rbtree_node<T> *p);
-template<class T>
-rbtree_node<T> *rb_decrement(rbtree_node<T> *p);
-
-/**
- * rbtree node traverses
- */
-
-template<class T>
-void rb_pre_order(rbtree_node<T> *p, stl::vector<T> &v);
-template<class T>
-void rb_in_order(rbtree_node<T> *p, stl::vector<T> &v);
-template<class T>
-void rb_post_order(rbtree_node<T> *p, stl::vector<T> &v);
-template<class T>
-void rb_level_order(rbtree_node<T> *p, stl::vector<T> &v);
-template<class T>
-void __rb_print(rbtree_node<T> *p, int prefix);
-template<class T>
-void rb_print(rbtree_node<T> *p);
-
-/**
- * rbtree iterator
- */
-template<class T, class Pointer, class Reference>
-struct rbtree_iterator {
-  typedef rbtree_iterator<T, Pointer, Reference> this_type;
-  typedef rbtree_iterator<T, T *, T &> iterator;
-  typedef rbtree_iterator<T, const T *, const T &> const_iterator;
-  typedef std::size_t size_type;
-  typedef std::ptrdiff_t difference_type;
-  typedef T value_type;
-  typedef rbtree_node<T> node_type;
-  typedef Pointer pointer;
-  typedef Reference reference;
-  typedef std::bidirectional_iterator_tag iterator_category;
-
-public:
-  node_type *node;
-
-public:
-  rbtree_iterator() : node(NULL) {}
-
-  explicit rbtree_iterator(const node_type *node)
-      : node(static_cast<node_type *>(const_cast<node_type *>(node))) {}
-
-  rbtree_iterator(const iterator &x) : node(x.node) {}
-
-  rbtree_iterator &operator=(const iterator &x) {
-    node = x.node;
-    return *this;
-  }
-
-  reference operator*() const { return node->value; }
-  pointer operator->() const { return &node->value; }
-
-  rbtree_iterator &operator++() {
-    node = static_cast<node_type *>(rb_increment(node));
-    return *this;
-  }
-
-  rbtree_iterator operator++(int) {
-    this_type tmp(*this);
-    node = static_cast<node_type *>(rb_increment(node));
-    return tmp;
-  }
-
-  rbtree_iterator &operator--() {
-    node = static_cast<node_type *>(rb_decrement(node));
-    return *this;
-  }
-
-  rbtree_iterator operator--(int) {
-    this_type tmp(*this);
-    node = static_cast<node_type *>(rb_decrement(node));
-    return tmp;
-  }
-};
-
-template<class T, class Pointer, class Reference>
-constexpr bool operator==(const rbtree_iterator<T, Pointer, Reference> &lhs,
-                          const rbtree_iterator<T, Pointer, Reference> &rhs) {
-  return lhs.node == rhs.node;
-}
 
 /**
  * rbtree
@@ -180,22 +69,7 @@ public:
   typedef const value_type &const_reference;
   typedef value_type *pointer;
   typedef const value_type *const_pointer;
-
-  typedef rbtree_iterator<value_type, value_type *, value_type &> iterator;
-  typedef rbtree_iterator<value_type, const value_type *, const value_type &> const_iterator;
-  typedef std::reverse_iterator<iterator> reverse_iterator;
-  typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
-
   typedef rbtree_node<value_type> node_type;
-
-  template<class Iter, class NodeType>
-  struct _insert_return_type {
-    Iter position;
-    bool inserted;
-    NodeType node;
-  };
-  typedef _insert_return_type<iterator, node_type> insert_return_type;
-
   typedef rbtree<T, Compare> this_type;
 
 private:
@@ -235,30 +109,6 @@ public:
   const node_type *root() const { return _root; }
 
   /**
-   * Iterators
-   */
-
-  iterator begin() noexcept {
-    return iterator(static_cast<node_type *>(rb_min_child(_root)));
-  }
-  const_iterator begin() const noexcept {
-    return const_iterator(static_cast<node_type *>(rb_min_child(_root)));
-  }
-  const_iterator cbegin() const noexcept {
-    return constiterator(static_cast<node_type *>(rb_min_child(_root)));
-  }
-
-  iterator end() noexcept {
-    return iterator(static_cast<node_type *>(rb_max_child(_root)->right));
-  }
-  const_iterator end() const noexcept {
-    return const_iterator(static_cast<node_type *>(rb_max_child(_root)->right));
-  }
-  const_iterator cend() const noexcept {
-    return const_iterator(static_cast<node_type *>(rb_max_child(_root)->right));
-  }
-
-  /**
    * Capacity
    */
 
@@ -270,11 +120,15 @@ public:
    */
 
   void clear() noexcept;
+
   void insert_fixup(rbtree_node<T> *p);
+  node_type *insert(rbtree_node<T> *p);
   node_type *insert(const value_type &value);
+
   void transplant(rbtree_node<T> *u, rbtree_node<T> *v);
-  void erase_fixup(rbtree_node<T> *p);
+  void erase_fixup(rbtree_node<T> *x);
   void erase(node_type *p);
+  void erase(const value_type &value);
 
   /**
    * Lookup
@@ -295,221 +149,6 @@ public:
   void print(rbtree_node<T> *p, int prefix);
   void print();
 };
-
-/**
- * ========================================================
- * implementations
- * ========================================================
- */
-
-/**
- * rbtree node implementation
- */
-
-template<class T>
-rbtree_node<T>::rbtree_node()
-    : parent(nullptr), left(nullptr), right(nullptr), color(RB_RED) {}
-
-template<class T>
-rbtree_node<T>::rbtree_node(const T &val, rbtree_node<T> *parent,
-                            rbtree_node<T> *left, rbtree_node<T> *right,
-                            rb_color color)
-    : value(val), parent(parent), left(left), right(right), color(color) {}
-
-template<class T>
-rbtree_node<T> *rbtree_node<T>::insert(const T &val, rb_side side) {
-  rbtree_node<T> *new_node = new rbtree_node(val, this);
-  if (side == RB_LEFT) {
-    left = new_node;
-  } else if (side == RB_RIGHT) {
-    right = new_node;
-  }
-  return this;
-}
-
-template<class T>
-rbtree_node<T> *rbtree_node<T>::attach(rbtree_node<T> *p, rb_side side) {
-  p->parent = this;
-  if (side == RB_LEFT) {
-    left = p;
-  } else if (side == RB_RIGHT) {
-    right = p;
-  }
-  return this;
-}
-
-/**
- * rbtree node functions
- */
-
-template<class T>
-rbtree_node<T> *rb_min_child(rbtree_node<T> *p) {
-  while (p->left != nullptr) {
-    p = p->left;
-  }
-  return p;
-}
-
-template<class T>
-rbtree_node<T> *rb_max_child(rbtree_node<T> *p) {
-  while (p->right != nullptr) {
-    p = p->right;
-  }
-  return p;
-}
-
-template<class T>
-rbtree_node<T> *rb_increment(rbtree_node<T> *p) {
-  if (p->right != nullptr) {
-    return rb_min_child<T>(p->right);
-  }
-  rbtree_node<T> *tmp = p->parent;
-  while (tmp != nullptr && p == tmp->right) {
-    p = tmp;
-    tmp = tmp->parent;
-  }
-  return tmp;
-}
-
-template<class T>
-rbtree_node<T> *rb_decrement(rbtree_node<T> *p) {
-  if (p->left != nullptr) {
-    return rb_max_child<T>(p->left);
-  }
-  rbtree_node<T> *tmp = p->parent;
-  while (tmp != nullptr && p == tmp->left) {
-    p = tmp;
-    tmp = tmp->parent;
-  }
-  return tmp;
-}
-
-/**
- * rbtree node traverses
- */
-
-template<class T>
-void rb_pre_order(rbtree_node<T> *p, stl::vector<T> &v) {
-  stack<rbtree_node<T> *> s;
-  while (true) {
-    while (p != nullptr) {
-      T val = p->value;
-      v.push_back(val);
-      s.push(p->right);
-      p = p->left;
-    }
-    if (s.empty()) {
-      break;
-    }
-    p = s.top();
-    s.pop();
-  }
-}
-
-template<class T>
-void rb_in_order(rbtree_node<T> *p, stl::vector<T> &v) {
-  stack<rbtree_node<T> *> s;
-  while (true) {
-    while (p != nullptr) {
-      s.push(p);
-      p = p->left;
-    }
-    if (s.empty()) {
-      break;
-    }
-    p = s.top();
-    s.pop();
-    T val = p->value;
-    v.push_back(val);
-    p = p->right;
-  }
-}
-
-template<class T>
-static void goto_left(stack<rbtree_node<T> *> &s) {
-  while (auto p = s.top()) {
-    if (p->left != nullptr) {
-      if (p->right != nullptr) {
-        s.push(p->right);
-      }
-      s.push(p->left);
-    } else {
-      s.push(p->right);
-    }
-  }
-  s.pop();
-}
-
-template<class T>
-void rb_post_order(rbtree_node<T> *p, stl::vector<T> &v) {
-  if (p == nullptr) {
-    return;
-  }
-  stack<rbtree_node<T> *> s;
-  s.push(p);
-  while (!s.empty()) {
-    if (s.top() != p->parent) {
-      goto_left(s);
-    }
-    p = s.top();
-    s.pop();
-    T val = p->value;
-    v.push_back(val);
-  }
-}
-
-template<class T>
-void rb_level_order(rbtree_node<T> *p, stl::vector<T> &v) {
-  if (p == nullptr) {
-    return;
-  }
-  stl::queue<rbtree_node<T> *> q;
-  q.push(p);
-  while (!q.empty()) {
-    p = q.front();
-    q.pop();
-    T val = p->value;
-    v.push_back(val);
-    if (p->left != nullptr) {
-      q.push(p->left);
-    }
-    if (p->right != nullptr) {
-      q.push(p->right);
-    }
-  }
-}
-
-template<class T>
-void __rb_print(rbtree_node<T> *p, int prefix) {
-  char prefix_str[prefix];
-  for (int i = 0; i < prefix; i++) {
-    prefix_str[i] = ' ';
-  }
-  prefix_str[prefix] = '\0';
-  if (p == nullptr) {
-    return;
-  }
-  std::cout << prefix_str;
-  if (p->parent == nullptr) {
-    std::cout << "--";
-  } else if (p == p->parent->left) {
-    std::cout << "->";
-  } else if (p == p->parent->right) {
-    std::cout << "=>";
-  }
-  std::cout << p->value << "\n";
-  __rb_print<T>(p->left, prefix + 2);
-  __rb_print<T>(p->right, prefix + 2);
-}
-
-template<class T>
-void rb_print(rbtree_node<T> *p) {
-  __rb_print<T>(p, 0);
-}
-
-/**
- * rbtree implementation
- */
 
 /**
  * Node operation
@@ -575,6 +214,7 @@ void rbtree<T, Compare>::rotate_right(rbtree_node<T> *p) {
 
 template<class T, class Compare>
 rbtree<T, Compare>::rbtree() : _root(), _size(0), comp(Compare()) {
+  nil = new rbtree_node<T>();
   nil->parent = nullptr;
   nil->left = nullptr;
   nil->right = nullptr;
@@ -605,6 +245,9 @@ rbtree<T, Compare>::~rbtree() {
 
 template<class T, class Compare>
 void rbtree<T, Compare>::clear() noexcept {
+  while (_size > 0) {
+    erase(_root);
+  }
 }
 
 template<class T, class Compare>
@@ -647,13 +290,12 @@ void rbtree<T, Compare>::insert_fixup(rbtree_node<T> *p) {
 }
 
 template<class T, class Compare>
-rbtree<T, Compare>::node_type *rbtree<T, Compare>::insert(const value_type &value) {
-  node_type *p = new rbtree_node<T>(value);
+rbtree<T, Compare>::node_type *rbtree<T, Compare>::insert(rbtree_node<T> *p) {
   node_type *y = nil;
   node_type *x = _root;
   while (x != nil) {
     y = x;
-    if (comp(value, x->value)) {
+    if (comp(p->value, x->value)) {
       x = x->left;
     } else {
       x = x->right;
@@ -662,7 +304,7 @@ rbtree<T, Compare>::node_type *rbtree<T, Compare>::insert(const value_type &valu
   p->parent = y;
   if (y == nil) {
     _root = p;
-  } else if (comp(value, y->value)) {
+  } else if (comp(p->value, y->value)) {
     y->left = p;
   } else {
     y->right = p;
@@ -673,6 +315,12 @@ rbtree<T, Compare>::node_type *rbtree<T, Compare>::insert(const value_type &valu
   p->color = RB_RED;
   insert_fixup(p);
   return p;
+}
+
+template<class T, class Compare>
+rbtree<T, Compare>::node_type *rbtree<T, Compare>::insert(const value_type &value) {
+  node_type *p = new rbtree_node<T>(value);
+  return insert(p);
 }
 
 /**
@@ -691,58 +339,58 @@ void rbtree<T, Compare>::transplant(rbtree_node<T> *u, rbtree_node<T> *v) {
 }
 
 template<class T, class Compare>
-void rbtree<T, Compare>::erase_fixup(rbtree_node<T> *p) {
-  rbtree_node<T> *w;
-  while (p != nil && p->color == RB_BLACK) {
-    if (p == p->parent->left) {
-      w = p->parent->right;
+void rbtree<T, Compare>::erase_fixup(rbtree_node<T> *x) {
+//  rbtree_node<T> *w;
+  while (x != _root && x->color == RB_BLACK) {
+    if (x == x->parent->left) {
+      rbtree_node<T> *w = x->parent->right;
       if (w->color == RB_RED) {
         w->color = RB_BLACK;
-        p->parent->color = RB_RED;
-        rotate_left(p->parent);
-        w = p->parent->right;
+        x->parent->color = RB_RED;
+        rotate_left(x->parent);
+        w = x->parent->right;
       }
       if (w->left->color == RB_BLACK && w->right->color == RB_BLACK) {
         w->color = RB_RED;
-        p = p->parent;
+        x = x->parent;
       } else if (w->right->color == RB_BLACK) {
         w->left->color = RB_BLACK;
         w->color = RB_RED;
         rotate_right(w);
-        w = p->parent->right;
+        w = x->parent->right;
       } else {
-        w->color = p->parent->color;
-        p->parent->color = RB_BLACK;
+        w->color = x->parent->color;
+        x->parent->color = RB_BLACK;
         w->right->color = RB_BLACK;
-        rotate_left(p->parent);
-        p = _root;
+        rotate_left(x->parent);
+        x = _root;
       }
     } else {
-      w = p->parent->left;
+      rbtree_node<T> *w = x->parent->left;
       if (w->color == RB_RED) {
         w->color = RB_BLACK;
-        p->parent->color = RB_RED;
-        rotate_right(p->parent);
-        w = p->parent->left;
+        x->parent->color = RB_RED;
+        rotate_right(x->parent);
+        w = x->parent->left;
       }
       if (w->right->color == RB_BLACK && w->left->color == RB_BLACK) {
         w->color = RB_RED;
-        p = p->parent;
+        x = x->parent;
       } else if (w->left->color == RB_BLACK) {
         w->right->color = RB_BLACK;
         w->color = RB_RED;
         rotate_left(w);
-        w = p->parent->left;
+        w = x->parent->left;
       } else {
-        w->color = p->parent->color;
-        p->parent->color = RB_BLACK;
+        w->color = x->parent->color;
+        x->parent->color = RB_BLACK;
         w->left->color = RB_BLACK;
-        rotate_right(p->parent);
-        p = _root;
+        rotate_right(x->parent);
+        x = _root;
       }
     }
   }
-  p->color = RB_BLACK;
+  x->color = RB_BLACK;
 }
 
 template<class T, class Compare>
@@ -776,6 +424,15 @@ void rbtree<T, Compare>::erase(rbtree::node_type *p) {
   if (y_color == RB_BLACK) {
     erase_fixup(x);
   }
+}
+
+template<class T, class Compare>
+void rbtree<T, Compare>::erase(const value_type &value) {
+  rbtree_node<T> *p = find(value);
+  if (p == nullptr) {
+    return ;
+  }
+  erase(p);
 }
 
 /**
@@ -930,32 +587,6 @@ void rbtree<T, Compare>::print(rbtree_node<T> *p, int prefix) {
 template<class T, class Compare>
 void rbtree<T, Compare>::print() {
   print(_root, 0);
-}
-
-/**
- * generate tree
- */
-
-static stl::rbtree_node<int> *generate_full_tree() {
-  stl::rbtree_node<int> *n = new stl::rbtree_node<int>(4);
-  stl::rbtree_node<int> *n1 = new stl::rbtree_node<int>(2);
-  n1->insert(1, stl::RB_LEFT);
-  n1->insert(3, stl::RB_RIGHT);
-  n->attach(n1, stl::RB_LEFT);
-  stl::rbtree_node<int> *n2 = new stl::rbtree_node<int>(6);
-  n2->insert(5, stl::RB_LEFT);
-  n2->insert(7, stl::RB_RIGHT);
-  n->attach(n2, stl::RB_RIGHT);
-  /**
-   * generated tree:
-   *
-   *       4
-   *      / \
-   *     2   6
-   *   / \  / \
-   *  1  3 5  7
-   */
-  return n;
 }
 
 }// namespace stl
--- test/tree_test.cpp
+++ test/tree_test.cpp
@@ -5,56 +5,19 @@
 #include "tree.h"
 #include <gtest/gtest.h>
 
-TEST(tree_test, node_functions) {
-  Log("==> test node functions...");
-  stl::rbtree_node<int> *n = stl::generate_full_tree();
-  stl::rbtree_node<int> *min, *max;
-  min = stl::rb_min_child(n);
-  EXPECT_EQ(min->value, 1);
-  max = stl::rb_max_child(n);
-  EXPECT_EQ(max->value, 7);
-  min = stl::rb_min_child(n->left);
-  EXPECT_EQ(min->value, 1);
-  max = stl::rb_max_child(n->left);
-  EXPECT_EQ(max->value, 3);
-  min = stl::rb_min_child(n->right);
-  EXPECT_EQ(min->value, 5);
-  max = stl::rb_max_child(n->right);
-  EXPECT_EQ(max->value, 7);
-  Log("==> pass!");
-}
-
-TEST(tree_test, node_traverses) {
-  Log("==> test node traverses...");
-  stl::rbtree_node<int> *n = stl::generate_full_tree();
-  stl::vector<int> v, ret;
-  stl::rb_pre_order(n, v);
-  ret.assign({4, 2, 1, 3, 6, 5, 7});
-  EXPECT_EQ(v, ret);
-  v.clear();
-  stl::rb_in_order(n, v);
-  ret.assign({1, 2, 3, 4, 5, 6, 7});
-  EXPECT_EQ(v, ret);
-  v.clear();
-  stl::rb_post_order(n, v);
-  ret.assign({1, 3, 2, 5, 7, 6, 4});
-  EXPECT_EQ(v, ret);
-  v.clear();
-  stl::rb_level_order(n, v);
-  ret.assign({4, 2, 6, 1, 3, 5, 7});
-  EXPECT_EQ(v, ret);
-  v.clear();
-  stl::rb_print(n);
-  Log("==> pass!");
-}
-
 TEST(tree_test, tree_insert) {
   Log("==> test tree insert...");
-  stl::rbtree<int> t;
+  stl::rbtree<int> t1;
   for (int i = 0; i < 18; i++) {
-    t.insert(i);
+    t1.insert(i);
   }
-  t.print();
+  t1.print();
+  stl::rbtree<int, std::greater<int>> t2;
+  for (int i = 0; i < 12; i++) {
+    t2.insert(i);
+  }
+  t2.print();
+  t2.in_order();
   Log("==> pass!");
 }
 
@@ -68,6 +31,18 @@ TEST(tree_test, tree_erase) {
   t.erase(t._root);
   t.erase(t._root->left);
   t.print();
+  t.in_order();
+  t.erase(0);
+  t.in_order();
+  t.erase(12);
+  t.in_order();
+  t.clear();
+  EXPECT_EQ(t.size(), 0);
+  for (int i = 0; i < 12; i++) {
+    t.insert(i);
+  }
+  t.in_order();
+  t.insert(1);
   t.in_order();
   Log("==> pass!");
 }
